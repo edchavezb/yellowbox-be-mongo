f112e90d80b4021188a85f871d337e0bfbd64209
edchavezb <edgp.chavez@gmail.com>
2022-11-29 21:13:23 -0600

    Add routes to get and update box by id

--- src/routes/box.ts
+++ src/routes/box.ts
@@ -3,7 +3,8 @@ import { BoxModel, IUserBox } from "../models/box";
 
 const routes = Router();
 
-routes.get("/", async (req, res) => {
+// Get a user's boxes
+routes.get("/userboxes", async (req, res) => {
   try {
     const { userId } = req.query;
     const boxes: IUserBox[] = await BoxModel.find({creator: userId as string}).exec();
@@ -14,12 +15,42 @@ routes.get("/", async (req, res) => {
   }
 });
 
+// Get a single box by id
+routes.get("/", async (req, res) => {
+  try {
+    const { boxId } = req.query;
+    const box: IUserBox | null = await BoxModel.findOne({_id: boxId as string}).exec();
+    return res.json(box);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Create a box
 routes.post("/", async (req, res) => {
   try {
     const userBox: Omit<IUserBox, "_id"> = req.body;
 
     const newBox = await BoxModel.create(userBox);
     return res.status(201).json(newBox);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Update a box
+routes.put("/", async (req, res) => {
+  try {
+    const { boxId } = req.query;
+    const replacementBox: IUserBox = req.body;
+
+    const updatedBox: IUserBox | null = await BoxModel.findOneAndReplace(
+      {_id: boxId as string},
+      replacementBox
+    ).exec();
+    return res.status(201).json(updatedBox);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Sorry, something went wrong :/" });
