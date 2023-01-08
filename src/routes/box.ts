53319374234dc2458d8315b4e423d69f73e9d153
edchavezb <edgp.chavez@gmail.com>
2023-01-07 18:29:34 -0600

    Move get user boxes endpoint to users

--- src/routes/box.ts
+++ src/routes/box.ts
@@ -3,18 +3,6 @@ import { BoxModel, IUserBox } from "../models/box";
 
 const routes = Router();
 
-// Get a user's boxes
-routes.get("/userboxes", async (req, res) => {
-  try {
-    const { userId } = req.query;
-    const boxes: IUserBox[] = await BoxModel.find({creator: userId as string}).exec();
-    return res.json(boxes);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Sorry, something went wrong :/" });
-  }
-});
-
 // Get a single box by id
 routes.get("/", async (req, res) => {
   try {
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { BoxModel, IUserBox } from "../models/box";
 import { UserModel, IUser } from "../models/user";
 
 const routes = Router();
 
+// Get a user's data by Spotify id
 routes.get("/", async (req, res) => {
   try {
     const { spotifyId } = req.query;
@@ -14,12 +16,25 @@ routes.get("/", async (req, res) => {
   }
 });
 
+// Create a new user
 routes.post("/", async (req, res) => {
   try {
     const user: Omit<IUser, "_id"> = req.body;
 
     const newUser = await UserModel.create(user);
     return res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Sorry, something went wrong :/" });
+  }
+});
+
+// Get a user's boxes
+routes.get("/:userId/boxes", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const boxes: IUserBox[] = await BoxModel.find({creator: userId as string}).exec();
+    return res.json(boxes);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Sorry, something went wrong :/" });
